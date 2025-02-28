c2a5363c96e750463cb59278c7feca0b5aad525c
Vivek Kumar <vivekvkc55@gmail.com>
2025-02-28 14:18:57 +0530

    BASE_DIR

--- techx/settings.py
+++ techx/settings.py
@@ -25,8 +25,7 @@ LOGGING['handlers']['file']['filename'] = "/tmp/logs/django.log"  # ✅ Use writ
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / "subdir".
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
